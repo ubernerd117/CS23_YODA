a279bd29ab629c4d56658f2a131d23ae8bf9d2ab
ubernerd117 <sourab.shaiks@gmail.com>
2023-04-01 05:28:14 -0400

    code that works

--- yoda/lib/destination.dart
+++ yoda/lib/destination.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class DestinationScreen extends StatefulWidget {
@@ -19,3 +21,40 @@ class _DestinationScreenState extends State<DestinationScreen> {
     );
   }
 }
+
+class OriginScreen extends StatefulWidget {
+  const OriginScreen({super.key});
+
+  @override
+  State<OriginScreen> createState() => _OriginScreenState();
+}
+
+class _OriginScreenState extends State<OriginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Which building are you at?'),
+        centerTitle: false,
+      ),
+      body: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          buildCard(),
+          SizedBox(width: 12),
+          buildCard(),
+          SizedBox(width: 12),
+          buildCard(),
+          SizedBox(width: 12),
+        ],
+      ),
+    );
+  }
+}
+
+Widget buildCard() => Container(
+      width: 400,
+      height: 300,
+      color: Colors.red,
+    );
--- yoda/lib/main.dart
+++ yoda/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'map.dart';
 import 'destination.dart';
+import 'dart:math';
 
 void main() {
   runApp(const Yoda());
@@ -17,9 +18,16 @@ class Yoda extends StatefulWidget {
 
 class _YodaState extends State<Yoda> {
   int currNavIndex = 0;
+  String funFact = 'Tap for a Falcon Fun Fact!';
 
   final List<Widget> screens = [
-    SizedBox(width: double.infinity, height: 500, child: MapScreen()),
+    OriginScreen(),
+    Column(
+      children: [
+        SizedBox(width: double.infinity, height: 700, child: MapScreen()),
+        factButton(funFacts: funFacts),
+      ],
+    ),
     DestinationScreen()
   ];
 
@@ -38,13 +46,24 @@ class _YodaState extends State<Yoda> {
         body: screens[currNavIndex],
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: currNavIndex,
+          selectedIconTheme: const IconThemeData(size: 30),
+          selectedItemColor: Colors.orange,
+          selectedLabelStyle: const TextStyle(color: Colors.orange),
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              label: 'Journey',
+              icon: Icon(
+                Icons.camera_alt,
+              ),
+              label: 'Location',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.house),
+              icon: Icon(
+                Icons.map,
+              ),
+              label: 'Directions',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_city),
               label: 'Destination',
             ),
           ],
@@ -53,6 +72,51 @@ class _YodaState extends State<Yoda> {
               currNavIndex = index;
             });
           },
+        ),
+      ),
+    );
+  }
+}
+
+List<String> funFacts = [
+  "BG track recruit, Nick Rose accidentally arrived Kentucky instead of Ohio, landed in Bowling Green, KY, and enrolled at WKU.",
+  "Freddy and Frieda have a 'beheading ceremony' at their last home game where their identities are revealed.",
+  "BGSU was initially a teacher training school named Bowling Green State Normal College.",
+  "First known photograph of the campus, taken in 1910 before buildings were erected.",
+  "Freddie is 16 years older than Frieda! Freddie was born in 1950, while Freda was born in 1966.",
+  "In order to become Freddie or Frieda Falcon, one must complete an online application."
+];
+
+class factButton extends StatefulWidget {
+  final List<String> funFacts;
+
+  factButton({required this.funFacts});
+
+  @override
+  _factButtonState createState() => _factButtonState();
+}
+
+class _factButtonState extends State<factButton> {
+  String _currentJoke = "Press the button for a joke!";
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      elevation: 0,
+      child: SizedBox(
+        width: double.infinity,
+        height: 78,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+              textStyle: const TextStyle(fontSize: 18)),
+          onPressed: () {
+            setState(() {
+              _currentJoke =
+                  widget.funFacts[Random().nextInt(widget.funFacts.length)];
+            });
+          },
+          child: Text(_currentJoke),
         ),
       ),
     );
