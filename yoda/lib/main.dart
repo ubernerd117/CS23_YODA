0b8c962c44c4bf235cfa28e1ca5069dfc4586502
ubernerd117 <sourab.shaiks@gmail.com>
2023-04-01 00:59:17 -0400

    split the code into diff files, to work

--- yoda/lib/main.dart
+++ yoda/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'map.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,29 +15,6 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
       ),
       home: MapScreen(),
-    );
-  }
-}
-
-class MapScreen extends StatefulWidget {
-  @override
-  _MapScreenState createState() => _MapScreenState();
-}
-
-class _MapScreenState extends State<MapScreen> {
-  static const _initialCameraPosition = CameraPosition(
-    target: LatLng(37.773972, -122.431297),
-    zoom: 11.5,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        initialCameraPosition: _initialCameraPosition,
-        ),
     );
   }
 }
